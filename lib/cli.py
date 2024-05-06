666f7ca8db6f466bcf5494a0ea50af8fd8a81ec4
Nimajavanbakht02 <nimabrah19@gmail.com>
2024-05-06 11:35:46 -0700

    adds basic cli and helpers

--- lib/cli.py
+++ lib/cli.py
@@ -1,31 +1,40 @@
 # lib/cli.py
-
-from helpers import exit_program, helper_1, helper_2, helper_3
-
+from helpers import (
+welcome,
+how_to_use,
+start,
+create_user,
+delete_user,
+exit_program, 
+)
 
 def main():
     while True:
         menu()
         choice = input("> ")
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            helper_1()
+        if choice == "1":
+            how_to_use()
         elif choice == "2":
-            helper_2()
+            start()
         elif choice == "3":
-            helper_3()
+            create_user()
+        elif choice == "4":
+            delete_user()
+        elif choice == "0":
+            exit_program()
         else:
-            print("Invalid choice")
+            print("Invalid choice!")
 
 
 def menu():
     print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
-    print("2. Some useful function")
-    print("3. Some useful function")
+    print("0. Exit")
+    print("1. How To Use")
+    print("2. Get Started")
+    print("3. Create User")
+    print("4. Delete User")
 
 
 if __name__ == "__main__":
+    welcome()
     main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,18 +1,65 @@
 # lib/helpers.py
 
+EXIT_WORDS = ["0", "exit", "quit"]
+def welcome():
+    print("""
+    _   ______  ___       __________  ___   ________ __ __________ 
+   / | / / __ )/   |     /_  __/ __ \/   | / ____/ //_// ____/ __ \
+  /  |/ / __  / /| |      / / / /_/ / /| |/ /   / ,<  / __/ / /_/ /
+ / /|  / /_/ / ___ |     / / / _, _/ ___ / /___/ /| |/ /___/ _, _/ 
+/_/ |_/_____/_/  |_|    /_/ /_/ |_/_/  |_\____/_/ |_/_____/_/ |_|  
+                                                                   """, style= "heading")
+    
+def how_to_use():
+    print("Welcome to NBA Stat Tracker!", style="subhead")
+    print("Here is how it works:", style="subhead")
+    print("1. Select 'Get Started'")
+    print("""2. Choose what statistics you would like to see: Choose from a team's current seasonal record
+          or their starting roster""")
+    print("3. Search what team whose statistics you'd like to see by their name.")
+    print("4. Have Fun!")
 
-def helper_1():
-    print("Performing useful function#1.")
+def start():
+    print("Please select an option:", style="subhead")
+    print("0. Exit the program")
+    print("1. Track Teams")
+    print("2. Track Teams Starting Roster")
+    print("3. Delete User")
 
 
-def helper_2():
-    print("Performing useful function#1.")
 
+def create_user():
+    # name = input("Enter your name: ").strip()
 
-def helper_3():
-    print("Performing useful function#1.")
+    # if name.lower() in EXIT_WORDS:
+    #     exit_program()
 
+    # player = User.find_by_name(name)
 
+    # if player is None:
+    #     new_player = User.create(name)
+    #     print(f"Welcome, {new_player.name}!", style="subhead")
+    #     play_game(new_player)
+    # else:
+    #     print(f"Welcome back, {player.name}!", style="subhead")
+    #     play_game(player)
+    pass
+
+def delete_user():
+    # name = input("Enter your name: ").strip()
+
+    # if name.lower() in EXIT_WORDS:
+    #     exit_program()
+    
+    # player = User.find_by_name(name)
+    # if player:
+    #     player.delete()
+    # else:
+    #     print(f"Could not find {name}.", style="subhead")
+    pass
 def exit_program():
     print("Goodbye!")
     exit()
+
+def play_game():
+    pass
