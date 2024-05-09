aa8ba1f29666d81f286e61d5cbc8b77c1725b9be
Pedro Alarcon <pedroa54@icloud.com>
2024-05-08 23:35:59 -0500

    adds data

---
+++ lib/playground.sql
@@ -0,0 +1,17 @@
+CREATE TABLE teams (
+id INTEGER PRIMARY KEY,
+team TEXT,
+city TEXT,
+wins INTEGER,
+losses INTEGER,
+championchips TEXT,
+pg TEXT,
+sg TEXT,
+sf TEXT,
+pf TEXT,
+c TEXT
+);
+
+
+INSERT INTO team (team,city,wins,losses,championchip,pg,sg,sf,pf,c)
+VALUES (Celtics, Boston, 64, 18, 17, Jrue Holiday, Derrick White, Jaylen Brown, Jayson Tatum, Kristaps Porzingis)
